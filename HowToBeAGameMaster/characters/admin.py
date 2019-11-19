a2d373d420c8adc24889dd3316560544bafa3d53
Patrik Friedl <patrik.friedl@gmail.com>
2019-11-19 01:07:52 +0100

    added basic character admin view

--- HowToBeAGameMaster/characters/admin.py
+++ HowToBeAGameMaster/characters/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Character, Skill, Inventory
+
+
+class SkillInline(admin.TabularInline):
+    model = Skill
+    extra = 1
+    min_num = 1
+    fields = ('talent', 'name', 'value')
+
+
+class InventoryInline(admin.StackedInline):
+    model = Inventory
+    verbose_name_plural = 'Inventory'
+    max_num = 1
+    min_num = 1
+    can_delete = False
+
+
+class CharacterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'kind', 'talent_act', 'talent_knowledge', 'talent_social')
+    list_filter = ('kind', )
+    fieldsets = (
+        ('CHARACTER INFORMATION', {
+            'fields': ('kind', 'portrait', 'name', 'gender', 'age', 'appearance',
+                       'profession', 'marital_status', 'religion')
+        }),
+        ('NOTES', {
+            'fields': ('game_master_notes', 'player_notes')
+        }),
+        ('WALLET', {
+            'fields': ('money',)
+        })
+    )
+    inlines = [SkillInline, InventoryInline]
+
+
+admin.site.register(Character, CharacterAdmin)
+
