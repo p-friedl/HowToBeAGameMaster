391fc6732890878396b9457dc8ebd38b0017185b
Patrik Friedl <patrik.friedl@gmail.com>
2019-11-20 00:50:47 +0100

    added inventory model test

--- HowToBeAGameMaster/characters/tests.py
+++ HowToBeAGameMaster/characters/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Inventory
+
+
+class InventoryModelTest(TestCase):
+
+    def test_inventory_verbose_name_plural(self):
+        self.assertEqual(str(Inventory._meta.verbose_name_plural), 'Inventories')
