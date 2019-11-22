b8457ac63e4e262ae697aa0b5b7679f83f67b711
Patrik Friedl <patrik.friedl@gmail.com>
2019-11-22 01:24:05 +0100

    added skill markup tests, rescue point test

--- HowToBeAGameMaster/characters/tests.py
+++ HowToBeAGameMaster/characters/tests.py
@@ -57,11 +57,50 @@ class CharacterModelTests(TestCase):
         self.assertEqual(test_character.talent_knowledge, 13)
         self.assertEqual(test_character.talent_social, 14)
 
+    def test_character_talent_calculation_skill_markups(self):
+        full_character = create_full_character()
+        character = full_character[0]
+        skill_talent_markup_diff_values = character.calculate_talents()
+        self.assertEqual(skill_talent_markup_diff_values, (13, 13, 14))
+        test_character = Character.objects.get(pk=character.pk)
+        for skill in test_character.skill_set.all():
+            if skill.name == 'Skill1' or skill.name == 'Skill2' or skill.name == 'Skill3':
+                self.assertEqual(skill.value, 100)
+            elif skill.name == 'Skill4' or skill.name == 'Skill5':
+                self.assertEqual(skill.value, 43)
+            else:
+                self.assertEqual(skill.value, 54)
+
+    def test_character_rescue_point_calculation(self):
+        full_character = create_full_character()
+        character = full_character[0]
+        character.calculate_talents()
+        character.calculate_rescue_points()
+        # without reload of character
+        self.assertEqual(character.rescue_points, 3)
+        # with reload of character
+        test_character = Character.objects.get(pk=character.pk)
+        self.assertEqual(test_character.rescue_points, 3)
+
 
 class SkillModelTests(TestCase):
     def test_skill_str_representation(self):
         skill = Skill(name='Test Skill')
         self.assertEqual(str(skill), 'Test Skill')
+
+    def test_skill_talent_markup_calc_exceeding(self):
+        full_character = create_full_character()
+        skills = full_character[1]
+        diff = skills[0].add_talent_markup(10)
+        self.assertEqual(skills[0].value, 100)
+        self.assertEqual(diff, 10)
+
+    def test_skill_talent_markup_calc_not_exceeding(self):
+        full_character = create_full_character()
+        skills = full_character[1]
+        diff = skills[3].add_talent_markup(10)
+        self.assertEqual(skills[3].value, 40)
+        self.assertEqual(diff, 0)
 
 
 class InventoryModelTests(TestCase):
