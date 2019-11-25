61ea1fab03ec1595c726dedb5a90b9de43759f9c
Patrik Friedl <patrik.friedl@gmail.com>
2019-11-26 00:52:21 +0100

    update CharacterForm, required and validation params

--- HowToBeAGameMaster/characters/forms.py
+++ HowToBeAGameMaster/characters/forms.py
@@ -1,16 +1,17 @@
 from django import forms
 
-from.models import Character
+from .models import Character
 
 
 class CharacterForm(forms.Form):
-    kind = forms.Select(choices=Character.KIND_CHOICES)
-    name = forms.CharField(max_length=100)
-    age = forms.IntegerField() # TODO: Valdiation logic research
-    gender = forms.Select(choices=Character.GENDER_CHOICES)
-    appearance = forms.CharField(max_length=500)
-    religion = forms.CharField(max_length=100)
-    profession = forms.CharField(max_length=100)
-    marital_status = forms.Select(choices=Character.MARITAL_STATUS_CHOICES)
+    kind = forms.CharField(widget=forms.Select(choices=Character.KIND_CHOICES), required=True)
+    name = forms.CharField(max_length=100, required=True)
+    age = forms.IntegerField(min_value=1, max_value=999, required=True)
+    gender = forms.CharField(widget=forms.Select(choices=Character.GENDER_CHOICES), required=True)
+    appearance = forms.CharField(max_length=500, required=True)
+    religion = forms.CharField(max_length=100, required=True)
+    profession = forms.CharField(max_length=100, required=True)
+    marital_status = forms.CharField(widget=forms.Select(choices=Character.MARITAL_STATUS_CHOICES), required=True)
     player_notes = forms.CharField(widget=forms.Textarea, max_length=2000, required=False)
     portrait = forms.ImageField(required=False)
+# TODO next: implement skills
