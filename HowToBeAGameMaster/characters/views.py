3a05416f7f21f2861eea18fa24438cab0940536c
Patrik Friedl <patrik.friedl@gmail.com>
2019-11-25 23:59:09 +0100

    Update views.py

--- HowToBeAGameMaster/characters/views.py
+++ HowToBeAGameMaster/characters/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+from .forms import CharacterForm
+
+
+class Create(View):
+    def get(self, request):
+        form = CharacterForm
+        return render(request, 'characters/create.html', {'form': form})
+
+    def post(self, request):
+        form = CharacterForm(request.POST)
+
+        if form.is_valid():
+            return render(request, 'characters/create.html', {
+                'text': request.POST['name']
+            })
