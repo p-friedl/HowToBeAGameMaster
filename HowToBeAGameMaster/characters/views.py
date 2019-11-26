0fcf103cec95644d7bfaf6b7d5b0fd5d6fb4969c
Patrik Friedl <patrik.friedl@gmail.com>
2019-11-26 23:00:26 +0100

    added Skill Form

--- HowToBeAGameMaster/characters/views.py
+++ HowToBeAGameMaster/characters/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render
 from django.views.generic import View
 
-from .forms import CharacterForm
+from .forms import CharacterForm, SkillForm
 
 
 class Create(View):
+    """
+    Class representing the character create view
+    """
     def get(self, request):
-        form = CharacterForm
-        return render(request, 'characters/create.html', {'form': form})
+        char_form = CharacterForm
+        skill_form = SkillForm
+        return render(request, 'characters/create.html',
+                      {'char_form': char_form, 'skill_form': skill_form})
 
     def post(self, request):
-        form = CharacterForm(request.POST)
+        char_form = CharacterForm(request.POST)
+        skill_form = SkillForm(request.POST)
 
-        if form.is_valid():
+        if char_form.is_valid() and skill_form.is_valid():
             return render(request, 'characters/create.html', {
                 'text': request.POST['name']
             })
