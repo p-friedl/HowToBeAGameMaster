d2fce8176677063c91ef5f1144ca3d52358acd71
patfri <patfri@QM-840G6-PATFRI.local>
2019-12-01 23:37:50 +0100

    skill_formset added prefix "skill"

--- HowToBeAGameMaster/characters/views.py
+++ HowToBeAGameMaster/characters/views.py
@@ -11,12 +11,12 @@ class Create(View):
 
     def get(self, request):
         char_form = CharacterForm()
-        skill_formset = SkillFormSet()
+        skill_formset = SkillFormSet(prefix='skill')
         return render(request, 'characters/create.html', {'char_form': char_form, 'skill_formset': skill_formset})
 
     def post(self, request):
         char_form = CharacterForm(request.POST)
-        skill_formset = SkillFormSet(request.POST)
+        skill_formset = SkillFormSet(request.POST, prefix='skill')
         if char_form.is_valid() and skill_formset.is_valid():
             char_form.save()
             skill_formset.instance = char_form.instance
