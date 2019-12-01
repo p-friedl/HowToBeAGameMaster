306ee092cc506aabec738d5bbede07ea0a14bddc
patfri <patfri@QM-840G6-PATFRI.local>
2019-12-01 23:36:15 +0100

    update code style

--- HowToBeAGameMaster/characters/forms.py
+++ HowToBeAGameMaster/characters/forms.py
@@ -3,8 +3,12 @@ from django.forms import modelform_factory, inlineformset_factory
 from .models import Character, Skill
 
 
-CharacterForm = modelform_factory(Character, fields=('kind', 'portrait', 'name', 'age', 'gender',
-                                                     'appearance', 'religion', 'profession', 'marital_status',
-                                                     'player_notes'))
+CharacterForm = modelform_factory(Character,
+                                  fields=('kind', 'portrait', 'name', 'age', 'gender', 'appearance', 'religion',
+                                          'profession', 'marital_status', 'player_notes'))
 
-SkillFormSet = inlineformset_factory(Character, Skill, fields=('talent', 'name', 'value'), extra=1)
+SkillFormSet = inlineformset_factory(Character,
+                                     Skill,
+                                     fields=('talent', 'name', 'value'),
+                                     extra=1,
+                                     max_num=80)
