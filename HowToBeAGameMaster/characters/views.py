caba8a3c59e9a20fe6d1cfa1c5658b239e331ca2
Patrik Friedl <patrik.friedl@gmail.com>
2019-12-11 01:03:47 +0100

    added auth to character creation

--- HowToBeAGameMaster/characters/views.py
+++ HowToBeAGameMaster/characters/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 from .forms import CharacterForm, SkillFormSet
 
@@ -9,15 +11,19 @@ class Create(View):
     Class representing the character create view
     """
 
+    @method_decorator(login_required)
     def get(self, request):
         char_form = CharacterForm()
         skill_formset = SkillFormSet(prefix='skill')
         return render(request, 'characters/create.html', {'char_form': char_form, 'skill_formset': skill_formset})
 
+    @method_decorator(login_required)
     def post(self, request):
         char_form = CharacterForm(request.POST)
         skill_formset = SkillFormSet(request.POST, prefix='skill')
         if char_form.is_valid() and skill_formset.is_valid():
+            char_form.save(commit=False)
+            char_form.instance.creator = request.user
             char_form.save()
             skill_formset.instance = char_form.instance
             skill_formset.save()
