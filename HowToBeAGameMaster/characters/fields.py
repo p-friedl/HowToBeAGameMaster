52fd1bf89c019446178ba99533c42ad8891526ad
Patrik Friedl <patrik.friedl@gmail.com>
2019-12-13 23:28:23 +0100

    added docstring to SkillValueField class

--- HowToBeAGameMaster/characters/fields.py
+++ HowToBeAGameMaster/characters/fields.py
@@ -2,6 +2,10 @@ from django.db.models import PositiveSmallIntegerField
 
 
 class SkillValueField(PositiveSmallIntegerField):
+    """
+    custom field class for Skill Values
+    sets a max_value of 100
+    """
     MAX_SKILL_VALUE = 100
 
     def formfield(self, **kwargs):
